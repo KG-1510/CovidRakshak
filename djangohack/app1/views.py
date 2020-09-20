ef98dfc51924d358a0992c00e2db88a72b850071
anushka17agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2020-09-20 07:31:14 +0530

    Addition of final Django file
    Addition of final Django file

--- djangohack/app1/views.py
+++ djangohack/app1/views.py
@@ -1,27 +1,25 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 import requests
 import json
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 
-def login(request):
-    form= UserCreationForm()
-    context = {'form': form}
-    return render(request, 'app1/login.html', context)
+from django.contrib.auth import password_validation
+from django.contrib.auth.models import User
+from django.shortcuts import render
 
-def signin(request):
-    form= UserCreationForm()
 
-    if request.method == 'POST':
-        form= UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-    context = {'form': form}
-    return render(request, 'app1/signin.html', context)
+
+def index(request):
+    return render(request, 'app1/index.html')
+
+
+
+
 
 def home(request):
     r = requests.get("http://gsx2json.com/api?id=1aLH0dQFW3mN5vSxJdhjdQuAMTKojCDn-7mk6CNH3W68&sheet=1")
     j=r.json()
-    
+      
     
     return render(request, 'app1/home.html', j)
 
@@ -30,14 +28,14 @@ def database(request):
     re= requests.get("http://gsx2json.com/api?id=104iTkc01Gnq79wi0ax1PmbDWZgwFqYGgiJovzF7zLMI&sheet=1")
     je= re.json()
     
-    return render(request, 'app1/database.html', je)
+    return render(request, 'app1/patient.html', je)
 
 def violations(request):
     req= requests.get("http://gsx2json.com/api?id=1P9rD3Ce0iHJqQHko6MrwNCGLTlx3jFt8DTldhsKOs_0&sheet=1")
     jeq= req.json()
     
     return render(request, 'app1/violations.html', jeq)
-
+ 
 
 
     
